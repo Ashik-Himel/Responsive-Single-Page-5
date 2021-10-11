13a5e5ed5055b263bf00f451b31181693c944574
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-10-11 23:24:51 +0600

    Counter and Preloader

---
+++ script.js
@@ -0,0 +1,18 @@
+// AOS JS
+AOS.init();
+AOS.init({
+    duration: 1000,
+})
+
+// PreLoader
+document.querySelector('body').onload = function() {
+    document.querySelector('.loading').style.display = 'none';
+}
+
+// CouterUp
+$(document).ready(function() {
+    $("counter").counterUp({
+        delay: 10,
+        time: 1500,
+    });
+});

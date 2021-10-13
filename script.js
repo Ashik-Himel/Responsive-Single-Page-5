72451c4db5de16271dce1eb12756c7d127e5a4db
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-10-13 11:04:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,28 @@
 AOS.init({
     duration: 1000
 })
-
 // PreLoader
 document.querySelector('body').onload = function() {
     document.querySelector('.loading').style.display = 'none';
 }
-
-// CouterUp
+// CounterUp
 $('.counter').counterUp({
     delay: 10,
-    time: 1500
+    time: 1000
+});
+// Isotope
+$('.tab-content').isotope({
+    itemSelector: '.item-image',
+    masonry: {},
+});
+
+$('.tab-titles span').click(function(){
+    $('.tab-titles span').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    $('.tab-content').isotope({
+        filter:selector
+    });
+    return false;
 });
